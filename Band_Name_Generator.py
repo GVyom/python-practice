8df34e693673a7d0f5570fcc03c44ceb205631b5
Vyom Goyal <154590962+GVyom@users.noreply.github.com>
2026-04-17 21:35:37 +0530

    Fix greeting message in Band Name Generator

--- Band_Name_Generator.py
+++ Band_Name_Generator.py
@@ -1,3 +1,4 @@
+print("Welcom to Band Name Generator. This application can help you generate name for your music band with two basic personal question.")
 city = input ("what is the name of the city you grew up in:")
 pet = input ("name of your first pet:")
 print("Your band name could be" " "+city+" "+pet)
