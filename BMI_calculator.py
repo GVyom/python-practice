2921819c920ba4b7aecbf6493c5794c55f4baa46
Vyom Goyal <154590962+GVyom@users.noreply.github.com>
2026-04-17 18:10:57 +0000

    BMI Calculators

---
+++ BMI_calculator.py
@@ -0,0 +1,7 @@
+print("This application can help you calculate your boday mass index")
+W = input("please enter your weight in Kgs:")
+H = input("please enter your height in meters")
+weight = int(W)
+hieght = float(H)
+BMI = weight/hieght**2
+print(BMI)
