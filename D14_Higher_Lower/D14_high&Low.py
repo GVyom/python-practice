fe571d4e0e33a416f337bf74c31509cf1cd6eac8
Vyom Goyal <vyomgoyal14@gmail.com>
2026-06-07 17:38:06 +0530

    Trying

--- D14_Higher_Lower/D14_high&Low.py
+++ D14_Higher_Lower/D14_high&Low.py
@@ -1,10 +1,10 @@
 from data import game_data
 import random
 
-def check_answer(A,B,answer,score):
+def check_answer(A,B,answer,score): #This check usually is to return True or False value
     if A['follower_count'] > B['follower_count'] and answer == 'lower':
         print("correct!")
-        return score + 1, True
+        return score + 1, True #used comma as we need to function to add value, plus return Boolean value
     if A['follower_count'] < B['follower_count'] and answer == 'higher':
         print('correct!')
         return score + 1, True
@@ -21,38 +21,23 @@ def play_game():
         if ready == 'y':
             break
     
-    
     score = 0
     A, B = random.sample(game_data, k=2)
-    while True:
+    while True: #This while is broken from the last "else: break statement"
         print(f'your score is {score}')
-        print(f"Compare A: {A['name']}, a {A['description']} from {A['country']}. {A['follower_count']}")
-        print(f"Compare B: {B['name']}, a {B['description']} from {B['country']}. {B['follower_count']}")
+        print(f"Compare A: {A['name']}, a {A['description']} from {A['country']}.")
+        print(f"Compare B: {B['name']}, a {B['description']} from {B['country']}.")
 
         answer = input('Please type higher or lower: ').lower()
-        score, correct = check_answer(A,B,answer,score)
-        if correct:
+        score, correct = check_answer(A,B,answer,score) #Output of check_answer function - Score + 1 also check (Boolean)
+        if correct: #correct is substituted with value such as True or False as result of check_asnwer output
             A = B
             while True:
                 B = random.choice(game_data)
-                if B != A:
-                    break
-        else:
+                if B != A: # if we B = A -> it will not run hence while will stay as True
+                    break # if B != A then while loop will break
+        else: # if correct False (False is only when answer is wrong)
                 print(f'final Score: {score}')
                 break
     
 play_game()
-#1. Generate Account A
-#2. Generate Account B
-#3. Display both accounts
-#4. Ask user for choice (A or B)
-#5. Compare follower counts
-#6. Check if user's choice is correct
-#7. If correct:
-#      - Increase score
-#      - Move B → A
-#      - Generate a new B
-#      - Repeat
-#8. If wrong:
-#      - End game
-#      - Display final score
